a4a0ea399019a57d54c0747a52af90aa825bf91c
Amin NAIRI <nairi.amin@gmail.com>
2020-05-29 09:51:44 +0200

    :sparkles: added more logs

--- main.mjs
+++ main.mjs
@@ -2,7 +2,11 @@ import {promises as fs} from "fs";
 import path from "path";
 import fetch from "node-fetch";
 
+console.log("Opening data.csv");
+
 fs.readFile(path.resolve("data.csv")).then(data => {
+    console.log("Successfully opened data.csv");
+
     const rows = [];
 
     const headers = [
@@ -15,6 +19,8 @@ fs.readFile(path.resolve("data.csv")).then(data => {
         "locales",
         "content"
     ];
+
+    console.log("Processing data.csv");
 
     data.toString().split("\n").filter(Boolean).forEach(line => {
         const row = {};
@@ -34,7 +40,11 @@ fs.readFile(path.resolve("data.csv")).then(data => {
         rows.push(row);
     });
 
+
     const ndjson = rows.reduce((raw, line) => `${raw}${JSON.stringify(line)}\n`, "") + "\n";
+
+    console.log("Successfully processed data.csv");
+    console.log("Ingesting processed data");
 
     fetch("http://elasticsearch:9200/groupe_2/_doc/_bulk", {
         method: "POST",
@@ -49,10 +59,12 @@ fs.readFile(path.resolve("data.csv")).then(data => {
         console.log("Failed to ingest data.");
     });
 
+    console.log("Writing processed data to data.ndjson");
+
     fs.writeFile(path.resolve("data.ndjson"), ndjson).then(() => {
-        console.log("Wrote data to data.ndjson successfully.");
+        console.log("Successfully wrote processed data to data.ndjson");
     }).catch(() => {
-        console.log("Failed to write data to data.ndjson.");
+        console.log("Failed to write processed data to data.ndjson");
     });
 }).catch(() => {
     console.error("Failed to open data.csv.");
